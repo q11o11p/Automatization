d3d068f71912da0b5da54ba9d07bcb62b1214fe2
Яша Лавка <qn0n1m0us@mail.ru>
2021-03-18 20:35:24 +0700

    init

---
+++ first/Ms.py
@@ -0,0 +1,12 @@
+import time
+from selenium import webdriver
+
+print('Введите адрес получателя:')
+mail = input()
+print('Введите сообщение:')
+text = input()
+
+browser = webdriver.Edge(executable_path=r'C:\проги\питон\moduls\msedgedriver.exe')
+browser.get('https://account.mail.ru/')
+time.sleep(30)
+browser.quit()
