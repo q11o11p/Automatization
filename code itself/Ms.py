9ba0d2b22c777ef43bb0d909822458a2e8c41c10
Яша Лавка <qn0n1m0us@mail.ru>
2021-04-03 01:21:17 +0700

    add: background mode was added again;

--- code itself/Ms.py
+++ code itself/Ms.py
@@ -1,11 +1,15 @@
 import time
 import sys
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-browser = webdriver.Edge(executable_path=r'C:\проги\питон\moduls\msedgedriver.exe')
+options = Options()
+options.headless = True
+
+browser = webdriver.Chrome(executable_path=r'C:\проги\питон\moduls\chromedriver.exe', chrome_options=options)
 browser.get('https://account.mail.ru/')
 print('Браузер успешно запустился.')
-time.sleep(10)
+time.sleep(35)
 print('Начинается авторизация...')
 
 login = browser.find_element_by_xpath('//input[@name="username"]')
@@ -24,15 +28,15 @@ print('Авторизация завершена.')
 submit2 = browser.find_element_by_tag_name('button')
 submit2.click()
 print('Заходим в аккаунт...')
-time.sleep(20)
+time.sleep(35)
 
 browser.get('https://e.mail.ru/compose/')
-time.sleep(5)
+time.sleep(15)
 print('Отправляем письмо...')
 
 who = browser.find_element_by_xpath('//input[@style="width: 12px;"]')
 who.send_keys(sys.argv[1])
-time.sleep(2)
+time.sleep(5)
 
 message = browser.find_element_by_xpath('//div[@role="textbox"]/div[1]')
 message.send_keys(' '.join(sys.argv[2:]))
